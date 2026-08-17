f0aa4bae11d38c18450fc7bdb5ef2a1ac5c9af38
coulibaly maimouna <coulmouna001@gmail.com>
2026-08-17 11:09:55 +0000

    commit

--- server.js
+++ server.js
@@ -1,74 +1,89 @@
-// import express from 'express';
-// import studentRouter from './routes/studentRoute.js'
-// import teacherRouter from './routes/teacherRoute.js'
-// import subjectRouter from './routes/subjectRoute.js'
-// import gradeRouter from './routes/gradeRoute.js'
-// import absenceRouter from './routes/absenceRoute.js'
-// import userRouter from './routes/userRoute.js'
-
-// const app = express();
-
-// app.use(express.json())
-
-// const PORT = 3000
-
-// app.use('/api/students', studentRouter);
-// app.use("/api/teachers", teacherRouter);
-// app.use("/api/subjects", subjectRouter);
-// app.use("/api/grades", gradeRouter);
-// app.use("/api/absences", absenceRouter);
-// app.use("/api/users", userRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`Serveur démarré sur http://localhost:${PORT}`);
-// });
-
-
-
-
-
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-import studentRouter from "./routes/studentRoute.js";
-import teacherRouter from "./routes/teacherRoute.js";
-import subjectRouter from "./routes/subjectRoute.js";
-import gradeRouter from "./routes/gradeRoute.js";
-import absenceRouter from "./routes/absenceRoute.js";
-import userRouter from "./routes/userRoute.js";
-import authRouter from "./routes/authRoute.js";
+
+import studentRouter from './routes/studentRoute.js'
+import teacherRouter from './routes/teacherRoute.js'
+import subjectRouter from './routes/subjectRoute.js'
+import gradeRouter from './routes/gradeRoute.js'
+import absenceRouter from './routes/absenceRoute.js'
+import userRouter from './routes/userRoute.js'
+
 
 const app = express();
-const PORT = 3000;
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-// Routes
-app.use("/api/auth", authRouter);
-app.use("/api/users", userRouter);
-app.use("/api/students", studentRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html')
+});
+
+app.get('/acceuil', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
+app.get('/absences', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/absences.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/dashboard.html'));
+});
+
+app.get('/grades', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/grades.html'));
+});
+
+app.get('/users', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/users.html'));
+});
+
+app.get('/profil', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/profil.html'));
+});
+
+app.get('/teacher', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/teacher.html'));
+});
+
+app.get('/students', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/students.html'));
+});
+
+app.get('/statistiques', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/statistiques.html'));
+});
+
+app.get('/subjects', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/html/subjects.html'));
+});
+
+const PORT = 3000
+
+
+
+
+
+
+
+
+
+
+
+app.use('/api/students', studentRouter);
 app.use("/api/teachers", teacherRouter);
 app.use("/api/subjects", subjectRouter);
 app.use("/api/grades", gradeRouter);
 app.use("/api/absences", absenceRouter);
+app.use("/api/users", userRouter);
 
-// Route d'accueil
-app.get("/", (req, res) => {
-  res.json({
-    message: "Bienvenue sur l'Application School Management",
-  });
-});
-
-// Gestion des routes inexistantes
-app.use((req, res) => {
-  res.status(404).json({
-    message: "Route introuvable",
-  });
-});
-
-// Démarrage du serveur
 app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+  console.log(`Serveur démarré sur http://localhost:${PORT}/acceuil`);
 });
